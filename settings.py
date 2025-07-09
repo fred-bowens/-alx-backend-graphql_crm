0d499520eeb603562b65c6652ec83e2bacf2a12d
Isaac Yawson <isaacayawson1996@gmail.com>
2025-07-09 08:38:58 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,13 @@ from celery.schedules import crontab
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
+
 
 INSTALLED_APPS = [
     ...
