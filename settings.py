32619549d2b9795c81cae33c32154e31e3b8cbff
Isaac Yawson <isaacayawson1996@gmail.com>
2025-07-09 08:36:03 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,14 @@
+from celery.schedules import crontab
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
 INSTALLED_APPS = [
     ...
     'graphene_django',
     'crm', 
     'django_filters'
     'django_celery_beat'
+    
 ]
